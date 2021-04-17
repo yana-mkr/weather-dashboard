f704673e0e6a662bd6b24cbd3c81025e273356c4
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-04-17 16:09:48 -0500

    initial files and pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// function to pull information from api 
+    // loop through days of the week to grab that data
+    //(make sure api is working and connected)
+
+// function to display the uv index
+
+// api request and creates elements on the page
+
+// search value box function -- get the value from the search box (querySelector.value)
+
