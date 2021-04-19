6f0ffcb42db8db2afce6179d5e3727858228cc52
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-04-18 19:23:27 -0500

    first functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,51 @@
-// function to pull information from api 
-    // loop through days of the week to grab that data
-    //(make sure api is working and connected)
+var searchInput = ("#search-input").value
+var searchButton =("#search-button");
+
+var todaysWeather = function (location) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=81054108cea086276c96966b6bf32e1c"
+
+    fetch(apiURL)
+    .then(function (response) {
+        if(response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                //1. get the icon from the data (weather element) data.list[0].weather.icon
+                //2. build the image url by concatenating the icon
+                //3. create the img element
+                //4. assing src to the url created in step 2
+                console.log(data);
+            })
+        }
+    })
+}
+
+var getForecast = function(location) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=81054108cea086276c96966b6bf32e1c"
+
+    fetch(apiURL)
+    .then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                //1. get the icon from the data (weather element) data.list[0].weather.icon
+                //2. build the image url by concatenating the icon
+                //3. create the img element
+                //4. assing src to the url created in step 2
+                console.log(data);
+            })
+        }
+    })
+}
+
+var submitLocation = function(searchInput) {
+    event.preventDefault();
+    searchButton.eventListener("click", todaysWeather, getForecast)
+    console.log(searchInput)
+}
+//todaysWeather("salt lake city");
+//getForecast("salt lake city");
+
+// in my onclick call both function with the city name
 
 // function to display the uv index
 
