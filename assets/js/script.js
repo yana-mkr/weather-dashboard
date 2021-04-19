ec7d42ecd83e03577d3150a286cec4f73bbbd8cd
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-04-18 20:30:48 -0500

    quick add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var searchInput = document.getElementById("search-input")
 var searchButton = document.getElementById("search-button")
+var cardText = document.querySelector(".card-text")
+var cityToday = document.querySelector("#city-today")
+var todayStats = document.querySelector("#today-stats")
 
 var todaysWeather = function (location) {
     var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=81054108cea086276c96966b6bf32e1c"
@@ -9,6 +12,7 @@ var todaysWeather = function (location) {
         if(response.ok) {
             console.log(response);
             response.json().then(function(data) {
+                todayStats.createElement("p")
                 //1. get the icon from the data (weather element) data.list[0].weather.icon
                 //2. build the image url by concatenating the icon
                 //3. create the img element
@@ -39,7 +43,6 @@ var getForecast = function(location) {
 
 
 searchButton.addEventListener("click", function(event){
-  
   todaysWeather(searchInput.value) 
   getForecast(searchInput.value)
 
@@ -47,10 +50,3 @@ searchButton.addEventListener("click", function(event){
     
 //todaysWeather("salt lake city");
 //getForecast("salt lake city");
-
-// function to display the uv index
-
-// api request and creates elements on the page
-
-// search value box function -- get the value from the search box (querySelector.value)
-
